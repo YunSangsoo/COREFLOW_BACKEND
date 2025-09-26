4d61689615ac7838a1734dd7aeeb1373a933e3c8
YunSangsoo <yss6024@naver.com>
2025-09-26 15:02:15 +0900

    수정

--- src/main/java/com/kh/coreflow/conference/controller/ConferenceRoomController.java
+++ src/main/java/com/kh/coreflow/conference/controller/ConferenceRoomController.java
@@ -49,7 +49,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/rooms")
+@RequestMapping("/api/rooms")
 @RequiredArgsConstructor
 @Slf4j
 public class ConferenceRoomController {
